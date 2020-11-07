b9d0b21631adf81d0d82a57f0be6917da0d4cd0c
Idrwla <ilyarredmi@gmail.com>
2020-11-07 20:10:15 +0600

    Работает чексум

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -12,9 +12,10 @@ import {getMatIconFailedToSanitizeLiteralError} from '@angular/material/icon';
 export class CreateComponent {
   regex = /^[0-9]+$/;
   errorMessage: string;
+  checksum: string;
   model = new FormGroup({
     id: new FormControl('', [Validators.required, Validators.pattern(this.regex),
-        Validators.maxLength(8), Validators.minLength(8)]),
+        Validators.maxLength(7), Validators.minLength(7)]),
     name: new FormControl('', [Validators.required , Validators.maxLength(50)]),
     state: new FormControl('', [Validators.required])
   });
@@ -39,6 +40,39 @@ export class CreateComponent {
     this.model.controls.id.setValue(  temp[0].id);
     this.model.controls.name.setValue(  temp[0].name);
     this.model.controls.state.setValue( temp[0].state);
+  }
+  digitMul(s): number{
+    try {
+      return parseInt(s, 10);
+    }finally {
+    }
+  }
+  checkSum(event): void{
+    const temp = event;
+    let sum = 0;
+    let tempChecksum = 0;
+    const firstDigitMul = this.digitMul(temp[0]) * 2;
+    const secondDigitMul = this.digitMul(temp[1]);
+    const thirdDigitMul = this.digitMul(temp[2]) * 2;
+    const fourthDigitMul = this.digitMul(temp[3]);
+    const fivesDigitMul = this.digitMul(temp[4]) * 2;
+    const sixDigitMul = this.digitMul(temp[5]);
+    const sevenDigitMul = this.digitMul(temp[6]) * 2;
+    const a = () =>  firstDigitMul > 10 ? firstDigitMul - 10 : firstDigitMul;
+    const b = () =>  secondDigitMul > 10 ? secondDigitMul - 10 : secondDigitMul;
+    const c = () =>  thirdDigitMul > 10 ? thirdDigitMul - 10 : thirdDigitMul;
+    const d = () =>  fourthDigitMul > 10 ? fourthDigitMul - 10 : fourthDigitMul;
+    const e = () =>  fivesDigitMul > 10 ? fivesDigitMul - 10 : fivesDigitMul;
+    const f = () =>  sixDigitMul > 10 ? sixDigitMul - 10 : sixDigitMul;
+    const g = () =>  sevenDigitMul > 10 ? sevenDigitMul - 10 : sevenDigitMul;
+    sum = 3 + a() + b() + c() + d() + e() + f() + g();
+    while ( ((sum + tempChecksum) % 10 !== 0)){
+      tempChecksum += 1;
+    }
+    console.log(sum);
+    console.log(tempChecksum);
+    tempChecksum = sum % 10 === 0 ? 0 : tempChecksum ;
+    this.checksum = tempChecksum.toString();
   }
   submitEdit(): void{
     if (!this.model.invalid){
@@ -89,6 +123,8 @@ export class CreateComponent {
       console.log(this.errorMessage);
     } else if (!this.model.invalid){
       let type: string;
+      this.checkSum(this.model.value.id);
+      this.model.controls.id.setValue(this.model.value.id + this.checksum);
       switch (this.model.value.id[0]){
         case '2':
           type = 'Крытый грузовой вагон';
